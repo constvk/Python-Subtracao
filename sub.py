15957880bb7c09e1a1aa0b6b4f2b3ac7f76a3255
constzada <72418922+constvk@users.noreply.github.com>
2021-04-23 16:03:30 -0300

    Create sub.py

---
+++ sub.py
@@ -0,0 +1,13 @@
+import os
+
+def sum(x, y):
+    return x - y
+
+print("Subtração de dois números")
+x = int(input("Digite um número: "))
+y = int(input("Digite outro número: "))
+
+print ("")
+print ("O resultado da Subtração é %d" %sum(x, y))
+
+os.system("pause")
